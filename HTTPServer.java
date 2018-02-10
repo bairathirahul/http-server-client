819e2a0590031d258256fb51ce0cf6fd6ca61be3
Rahul Bairathi <rbairath@uncc.edu>
2018-02-09 23:26:33 -0500

    Added codes for server implementation

--- HTTPServer.java
+++ HTTPServer.java
@@ -1,75 +1,71 @@
+import java.io.IOException;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.SocketTimeoutException;
+import java.net.Socket;
+import java.net.ServerSocket;
+
 public class HTTPServer extends Thread {
-	ServerSocket serverSocket;
-	public HTTPServer(int port, int timeout) {
-		serverSocket = new ServerSocket(port);
-		serverSocket.setSOTimeout(timeout);
-	}
+    private ServerSocket serverSocket;
 
-	public void run() {
-		while(true) {
-			Socket socket;
-			DataInputStream dis;
-			DataOutputStream dos;
-			try {
-				Socket socket = serverSocket.accpet();
-				DataInputStream dis = new DataInputStream(socket);
+    private HTTPServer(int port) throws IOException {
+        serverSocket = new ServerSocket(port);
+    }
 
-				// Todo: Parse the incoming packet
+    public void run() {
+        while (true) {
+            Socket socket = null;
+            DataInputStream dis = null;
+            DataOutputStream dos = null;
+            try {
+                socket = serverSocket.accept();
+                dis = new DataInputStream(socket.getInputStream());
 
-				DataOutputStream dos = new DataOutputStream(socket);
-				// Write some output
-				out.write("")
-			} catch(SocketTimeoutException e) {
-				// Todo: Generate socket timeout exception
-			} catch(IOException e) {
-				// Implement IO Exception
-			} finally {
-				try {
-					// Release resources
-					if(dis != null) {
-						dis.close();
-					}
-					if(dos != null) {
-						dos.close();
-					}
-					if(socket != null) {
-						socket.close();
-					}
-				} catch(Exception e) {
-					// Do nothing
-				}
-			}
-		}
-	}
+                // Todo: Parse the incoming packet
 
-	
-	public static void main(String[] args) {
-		int port = 1025;
-		if(args.length > 1) {
-			try {
-				port = Integer.parseInt(args[1]);
-			} catch(NumberFormatException ex) {
-				// Todo: Generate invalid port error
+                dos = new DataOutputStream(socket.getOutputStream());
+                // Write some output
+                //dos.write("");
+            } catch (SocketTimeoutException e) {
+                // Todo: Generate socket timeout exception
+            } catch (IOException e) {
+                // Implement IO Exception
+            } finally {
+                try {
+                    // Release resources
+                    if (dis != null) {
+                        dis.close();
+                    }
+                    if (dos != null) {
+                        dos.close();
+                    }
+                    if (socket != null) {
+                        socket.close();
+                    }
+                } catch (Exception e) {
+                    // Do nothing
+                }
+            }
+        }
+    }
 
-			}
-		}
+    public static void main(String[] args) {
+        int port = 1025;
+        if (args.length > 1) {
+            try {
+                port = Integer.parseInt(args[1]);
+            } catch (NumberFormatException ex) {
+                System.out.println("Invalid port number provided");
+            }
+        }
 
-		int timeout = 1000;
-		if(args.length > 2) {
-			try {
-				timeout = Integer.parseInt(args[2]);
-			} catch(NumberFormatException ex) {
-				// Todo: Generate invalid port error
-
-			}
-		}
-
-		ServerSocket socket;
-		try{
-			socket = new ServerSocket(port);
-		} catch(IOException e) {
-			System.out.println("Server terminated");
-			e.printStackTrace();
-		}
-	}
+        HTTPServer server = null;
+        try {
+            server = new HTTPServer(port);
+            server.run();
+        } catch (IOException e) {
+            System.out.println("Server cannot be initiated. See below for error details");
+            e.printStackTrace();
+        }
+    }
 }
